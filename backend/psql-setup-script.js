fb86d2b608964db52df4684032de8b7ea5964571
Shelbey Casalena <shelbey.casalena@gmail.com>
2024-03-29 15:51:30 -0700

    try to fix package.json for render

--- backend/psql-setup-script.js
+++ backend/psql-setup-script.js
@@ -1,4 +1,4 @@
-const { sequelize } = require("./config");
+const { sequelize } = require("./config/database");
 
 sequelize.showAllSchemas({ logging: false }).then(async (data) => {
   if (!data.includes(process.env.SCHEMA)) {
