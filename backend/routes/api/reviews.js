6ef1acfbc2161599ef37d39220a89e86b36dbfc2
Shelbey Casalena <shelbey.casalena@gmail.com>
2024-04-04 14:46:43 -0700

    get all review of current user

--- backend/routes/api/reviews.js
+++ backend/routes/api/reviews.js
@@ -23,12 +23,13 @@ const {
   validateCreateSpot,
   validateCreateReview,
 } = require("../../utils/validation");
+const review = require("../../db/models/review");
 
 const router = express.Router();
 
 router.get("/current", requireAuth, async (req, res, next) => {
   const userId = req.user.id;
-  const allReviews = await Review.findAll({
+  const allReviewsWithUser = await Review.findAll({
     where: {
       userId: userId,
     },
@@ -38,9 +39,78 @@ router.get("/current", requireAuth, async (req, res, next) => {
         attributes: ["id", "firstName", "lastName"],
       },
     ],
+    attributes: [
+      "id",
+      "spotId",
+      "userId",
+      "review",
+      "stars",
+      "createdAt",
+      "updatedAt",
+    ],
   });
 
-  console.log(allReviews);
+  const newBody = { Reviews: [] };
+
+  for (let review in allReviewsWithUser) {
+    const reviewCopy = { ...allReviewsWithUser[review].dataValues };
+    const images = await ReviewImage.findAll({
+      where: { reviewId: reviewCopy.id },
+    });
+
+    const spot = await Spot.findByPk(reviewCopy.spotId, {
+      attributes: {
+        exclude: ["createdAt", "updatedAt", "description"],
+      },
+      include: [
+        {
+          model: SpotImage,
+          where: { preview: true },
+          attributes: ["url"],
+          limit: 1,
+        },
+      ],
+    });
+    let spotCopy = { ...spot.dataValues };
+    // SET PREVIEW IMAGE ON SPOT
+    if (spotCopy.SpotImages[0]) {
+      spotCopy.previewImage = spotCopy.SpotImages[0].url;
+    } else {
+      spotCopy.previewImage = null;
+    }
+    delete spotCopy.SpotImages;
+
+    // FORMAT REVIEW COPY
+    reviewCopy.stars = parseInt(reviewCopy.stars);
+    reviewCopy.createdAt = reviewCopy.createdAt
+      .toISOString()
+      .split("T")
+      .join(" ")
+      .split("Z")
+      .join(" ")
+      .split(".")
+      .slice(0, -1)
+      .join(" ");
+
+    reviewCopy.updatedAt = reviewCopy.updatedAt
+      .toISOString()
+      .split("T")
+      .join(" ")
+      .split("Z")
+      .join(" ")
+      .split(".")
+      .slice(0, -1)
+      .join(" ");
+
+    const newReview = {
+      ...reviewCopy,
+      ReviewImages: [images.dataValues],
+      Spot: { ...spotCopy },
+    };
+    newBody.Reviews.push(newReview);
+  }
+  // console.log("NEW BODY", newBody);
+  res.json(newBody);
 });
 
 module.exports = router;
