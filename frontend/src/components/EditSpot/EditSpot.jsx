13b28589d395976f3abceb980b4a8ef27cb8dd14
Shelbey Casalena <shelbey.casalena@gmail.com>
2024-04-29 01:10:42 -0700

    fixed manage spots

--- frontend/src/components/EditSpot/EditSpot.jsx
+++ frontend/src/components/EditSpot/EditSpot.jsx
@@ -3,19 +3,31 @@ import { useParams } from "react-router-dom";
 import { useNavigate } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
 import { getSpotById } from "../../store/currentSpot";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { updateSpot } from "../../store/currentSpot";
 // import "..CreateASpot/";
 function EditSpot() {
   const navigate = useNavigate();
   const dispatch = useDispatch();
+  const [loadForm, setLoadForm] = useState(false);
   const spotId = useParams().spotId;
 
   const currentSpot = useSelector((state) => state.currentSpot);
   console.log("CURRENT SPOT IN EDIT SPOT", currentSpot);
+
   useEffect(() => {
     dispatch(getSpotById(spotId));
   }, [dispatch, spotId]);
+
+  useEffect(() => {
+    if (
+      currentSpot &&
+      Object.keys(currentSpot).length > 0 &&
+      currentSpot.id == spotId
+    ) {
+      setLoadForm(true);
+    }
+  }, [setLoadForm, spotId, currentSpot]);
 
   const submitToParent = async (payload) => {
     console.log("PAYLOAD IN PARENT", payload);
@@ -28,18 +40,18 @@ function EditSpot() {
 
   return (
     <>
-      {/* {currentSpot.id == spotId && ( */}
-      <div className="below-nav">
-        <h1>Update your Spot</h1>
-        {Object.keys(currentSpot).length > 0 && (
-          <CreateOrEditSpotForm
-            submitToParent={submitToParent}
-            currentSpot={currentSpot}
-            hideImages={true}
-          />
-        )}
-      </div>
-      {/* )} */}
+      {loadForm && (
+        <div className="below-nav">
+          <h1>Update your Spot</h1>
+          {Object.keys(currentSpot).length > 0 && (
+            <CreateOrEditSpotForm
+              submitToParent={submitToParent}
+              currentSpot={currentSpot}
+              hideImages={true}
+            />
+          )}
+        </div>
+      )}
     </>
   );
 }
